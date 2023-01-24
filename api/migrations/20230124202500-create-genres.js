dacdc8fbc307a85f4d3ab9b7e80288a84119d583
Gustavo <jgustavoas@gmail.com>
2023-01-24 20:33:54 -0300

    Database migrations

---
+++ api/migrations/20230124202500-create-genres.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('genres', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      genre: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('genres');
+  },
+};
