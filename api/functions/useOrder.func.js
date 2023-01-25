d78400e729400b928d5ba6783922e87ae19d4538
Gustavo <jgustavoas@gmail.com>
2023-01-24 21:17:37 -0300

    Utility functions

---
+++ api/functions/useOrder.func.js
@@ -0,0 +1,22 @@
+const OPTIONS = require('../objects/options.obj');
+
+/*
+  Check wich model has the column indicated by the parameter "req.query.by" to sort the table.
+  
+  Examples: 
+  BASE_URL/albums?by=album
+  BASE_URL/tracks?by=artist&sort=DESC  
+*/
+module.exports = (req) => {
+  const { MODEL } = req.params;
+  const { by, sort } = req.query;
+  const order = [by, sort || 'ASC'];
+
+  if (!by) return null;
+
+  OPTIONS[MODEL].include?.forEach((inc) => {
+    if (inc.attributes.includes(by)) order.splice(0, 0, inc);
+  });
+
+  return [order];
+};
---
+++ api/functions/useWhere.func.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-param-reassign */
+const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+const OPTIONS = require('../objects/options.obj');
+
+module.exports = req => {
+  const { MODEL } = req.params;
+  const { attributes, include } = OPTIONS[MODEL];
+  const { by, sort, offset, limit, ...cols } = req.query;
+  const andColumns = {};
+
+  if (!cols) return null;
+
+  Object.entries(cols).forEach(and => {
+    const [col, val] = and;
+
+    const found = attributes.find(column => column === col);
+    if (found) andColumns[col] = { [Op.like]: `%${val}%` };
+
+    if (include) {
+      include.forEach(model => {
+        const foundModel = model.attributes.some(column => column === col);
+
+        if (foundModel) {
+          model.where = { [col]: { [Op.like]: `%${val}%` } }; // #1
+          model.required = true; // #1
+        }
+      });
+    }
+  });
+
+  return {
+    [Op.and]: andColumns,
+  };
+};
+
+/*
+  Examples:
+
+  1) This endpoint fetches both Pearl Jam "Oceans" and Lighthouse Family "Ocean Drive" tracks:
+  BASE_URL/tracks?track=ocean
+
+  2) This one fetches the same as the previous one but sorted by artist in descending order:
+  BASE_URL/tracks?by=artist&sort=DESC&track=ocean
+
+  3) The following endpoint fetches only Pearl Jam "Oceans":
+  BASE_URL/tracks?track=ocean&artist=jam
+
+  4) This last endpoint fetches only Lighthouse Family "Ocean Drive":
+  BASE_URL/tracks?track=ocean&artist=fam
+*/
+
+/*
+    #1 Check about "options.include[].where" and "options.include[].required":
+    https://sequelize.org/v5/class/lib/model.js~Model.html#static-method-findAll
+*/
