9d75f3e4ecf6e6379d0db41adad452b90f686855
Gustavo <jgustavoas@gmail.com>
2023-01-24 21:18:21 -0300

    Update route file

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { Create, Read, Update, Delete } = require('../controllers/Universal');
 
 const router = express.Router();
 
-/* GET home page. */
+/* home page */
 router.get('/', (req, res, next) => {
-  res.json({ title: 'Express' });
+  res.json({
+    title: 'Music and Movies 2',
+    text: 'Welcome to Music and Movies 2',
+  });
 });
+
+/* models page */
+router.post('/:MODEL', Create);
+router.get('/:MODEL', Read);
+router.patch('/:MODEL/:ID', Update);
+router.delete('/:MODEL/:ID', Delete);
 
 module.exports = router;
