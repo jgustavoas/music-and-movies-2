f378051080f42e9c23338e05285facee4ae865b6
Gustavo <jgustavoas@gmail.com>
2023-01-25 21:41:29 -0300

    Header component

---
+++ app/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import '../styles/Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div>Music and Movies 2</div>
+      <nav>
+        <button>Artists</button>
+        <button>Albums</button>
+        <button>Tracks</button>
+        <button>Movies</button>
+        <button>Genres</button>
+      </nav>
+    </header>
+  );
+}
