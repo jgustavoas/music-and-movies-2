0efb38d89b26f0c62be67a64e488336549c9e607
Gustavo <jgustavoas@gmail.com>
2023-01-26 21:46:39 -0300

    Index file for route files

---
+++ app/src/routes/index.jsx
@@ -0,0 +1,19 @@
+import ErrorPage from './error-page';
+import Root from './root';
+import Home from './home';
+import Artists from './artists';
+import Albums from './albums';
+import Tracks from './tracks';
+import Movies from './movies';
+import Genres from './genres';
+
+export default {
+  ErrorPage: <ErrorPage />,
+  Root: <Root />,
+  Home: <Home />,
+  Artists: <Artists />,
+  Albums: <Albums />,
+  Tracks: <Tracks />,
+  Movies: <Movies />,
+  Genres: <Genres />,
+};
