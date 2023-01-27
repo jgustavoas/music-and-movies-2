9fbc45e47051dd8e47075f6eeff0363a09734555
Gustavo <jgustavoas@gmail.com>
2023-01-26 21:38:34 -0300

    Implement react-router-dom

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import indexRoutes from './routes';
 import './styles/index.css';
+
+const { ErrorPage, Home, Artists, Albums, Tracks, Movies, Genres } =
+  indexRoutes;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: ErrorPage,
+    children: [
+      {
+        path: '/',
+        element: Home,
+      },
+      {
+        path: '/artists',
+        element: Artists,
+      },
+      {
+        path: '/albums',
+        element: Albums,
+      },
+      {
+        path: '/tracks',
+        element: Tracks,
+      },
+      {
+        path: '/movies',
+        element: Movies,
+      },
+      {
+        path: '/genres',
+        element: Genres,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
