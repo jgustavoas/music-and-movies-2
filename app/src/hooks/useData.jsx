5a53140409797c0e2c27fae7df910838a4fe0e19
Gustavo <jgustavoas@gmail.com>
2023-01-31 21:31:12 -0300

    Create custom hook to fetch data

---
+++ app/src/hooks/useData.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export function useData() {
+  const { pathname } = document.location;
+  const baseURL = 'http://localhost:3000';
+  const endpoint = `${baseURL}${pathname}`;
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (pathname !== '/') {
+      let ignore = false;
+
+      fetch(endpoint)
+        .then(response => response.json())
+        .then(json => {
+          if (!ignore) {
+            setData(json);
+          }
+        });
+
+      return () => {
+        ignore = true;
+      };
+    } else return null;
+  }, []);
+
+  console.log('data :>> ', data);
+
+  return data;
+}
