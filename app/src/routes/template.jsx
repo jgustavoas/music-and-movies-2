a515122f76f80922db305e73995d70e860740650
Gustavo <jgustavoas@gmail.com>
2023-01-31 21:31:33 -0300

    Use custom hook

--- app/src/routes/template.jsx
+++ app/src/routes/template.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import { useData } from '../hooks/useData';
 import Table from '../Components/Table';
 import { updatePageTitle } from '../functions/page.func';
 
 export default function Template({ title, columns }) {
   document.title = updatePageTitle(title);
+  useData();
 
   return (
     <>
