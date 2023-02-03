43a17cc64e6745a7e4b269cc6b6c9c4a21898b72
Gustavo <jgustavoas@gmail.com>
2023-02-02 21:49:57 -0300

    Move custom hook to Table component

--- app/src/routes/template.jsx
+++ app/src/routes/template.jsx
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types';
-import { useData } from '../hooks/useData';
 import Table from '../Components/Table';
 import { updatePageTitle } from '../functions/page.func';
 
 export default function Template({ title }) {
   document.title = updatePageTitle(title);
-  const data = useData();
 
   return (
     <>
-      <h1>{title}</h1> <Table data={data} />
+      <h1>{title}</h1> <Table />
     </>
   );
 }
