c6c5a63dda9e17f3038031e4998a4d345cf2c26d
Gustavo <jgustavoas@gmail.com>
2023-02-07 20:14:39 -0300

    Draft modal component

---
+++ app/src/Components/Modal.jsx
@@ -0,0 +1,10 @@
+export default function Modal() {
+  return (
+    <div id="modal">
+      <header>
+        Modal title <button>X</button>
+      </header>
+      <div id="modal-content">FORM</div>
+    </div>
+  );
+}
