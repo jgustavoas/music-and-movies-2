ac1030e81d8b3749e51f76bd67dd3f63db77bce5
Gustavo <jgustavoas@gmail.com>
2023-02-07 21:24:30 -0300

    Import style of Modal component

--- app/src/Components/Modal.jsx
+++ app/src/Components/Modal.jsx
@@ -1,10 +1,19 @@
+import { useModal } from '../hooks/useModal';
+import '../styles/Modal.css';
+
 export default function Modal() {
+  const open = useModal();
+
+  if (!open) return null;
+
   return (
     <div id="modal">
-      <header>
-        Modal title <button>X</button>
-      </header>
-      <div id="modal-content">FORM</div>
+      <div id="modal-container">
+        <div className="header">
+          Modal title <button>X</button>
+        </div>
+        <div id="modal-content">FORM</div>
+      </div>
     </div>
   );
 }
