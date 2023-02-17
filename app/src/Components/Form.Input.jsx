48ccbbd3e6030e5eca6022f08da77786d7073529
Gustavo <jgustavoas@gmail.com>
2023-02-16 22:04:47 -0300

    Update <select> input value on Change event

--- app/src/Components/Form.Input.jsx
+++ app/src/Components/Form.Input.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import useData from '../hooks/useData';
 
+function evChange(ev, setInputValue) {
+  setInputValue(ev.target.value);
+}
+
 export default function Input({ props }) {
-  const { type, name, index, value } = props;
-  const id = `${type}_${name}`;
+  const { type, name, index, formData } = props;
+  const inputID = `${type}_${name}`;
+
+  const columnValue = formData?.[name];
+  const notObject = typeof columnValue !== 'object' || !columnValue;
+  const foreignID = notObject ? '' : formData[name].id;
+  const value = notObject ? columnValue : foreignID;
+
+  const [inputValue, setInputValue] = useState(value);
 
   switch (type) {
     case 'select': {
@@ -15,10 +27,15 @@ export default function Input({ props }) {
 
       return (
         <section key={`section-input_${index}`}>
-          <label key={`label_${index}`} htmlFor={id}>
+          <label key={`label_${index}`} htmlFor={inputID}>
             {name}
           </label>
-          <select name={name} id={id} defaultValue={value}>
+          <select
+            name={name}
+            id={inputID}
+            value={inputValue}
+            onChange={ev => evChange(ev, setInputValue)}
+          >
             <option value="">--- Select {name} ---</option>
             {options}
           </select>
@@ -29,14 +46,14 @@ export default function Input({ props }) {
       return (
         <>
           <section key={`section-input_${index}`}>
-            <label key={`label_${index}`} htmlFor={id}>
+            <label key={`label_${index}`} htmlFor={inputID}>
               {name}
             </label>
             <input
               key={`input_${index}`}
               type={type}
               name={name}
-              id={id}
+              id={inputID}
               defaultValue={value}
             />
           </section>
